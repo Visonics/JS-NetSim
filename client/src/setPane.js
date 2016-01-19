a6d8e3c6071b362ba37981dec9cd0cd61e2bd1b7
Omar Shaikh <oshaikh13@gmail.com>
2016-01-19 21:50:35 +0300

    Added filepicker to new layout. Intergrated old netsim code

--- client/src/setPane.js
+++ client/src/setPane.js
@@ -1,8 +1,21 @@
+var width = 700;
+var height = 500;
+
 var setPane = function(target, options){
   console.log(options);
 
   if (options.wipeOnNewLoad) {
     target.selectAll("*").remove();
+  }
+
+  if (options.width && options.height) {
+    svg.attr("width", options.width)
+    .attr("id", targetSvgId)
+    .attr("height", options.height);
+  } else {
+    svg.attr("width", width)
+    .attr("id", targetSvgId)
+    .attr("height", height);
   }
 
 };
