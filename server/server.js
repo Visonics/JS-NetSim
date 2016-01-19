546632e492206105a5a53a2ef7c17240adb22ea9
Omar Shaikh <oshaikh13@gmail.com>
2016-01-19 17:54:20 +0300

    Added controller and fetching of json. Added .gitignore

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-server.js
+var controller = require('./controller.js');
+var express = require('express');
+var app = express();
+
+app.get('/data/:name', controller.getData);
+app.get('/data/', controller.getDataList);
+
+
+app.get('/', function(req, res){
+  res.send('JS-Netsim.')
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
+
