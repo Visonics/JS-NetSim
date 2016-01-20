39537c89225b790d75162b97817690a8a8ec1d90
Omar Shaikh <oshaikh13@gmail.com>
2016-01-20 08:19:59 +0300

    Added server fetching on client

--- client/src/filePicker.js
+++ client/src/filePicker.js
@@ -1,7 +1,56 @@
 // This will change based on what a user picks...
+
+var serverURL = "http://localhost:3000"
+
 var wipeOnNewLoad = false;
 
+var JSONLoaded = false;
+
+var makeRadioDiv = function(name) {
+  return '<div class="radio"><label><input type="radio" name="optradio" value="' + name + '">' + name + '</label></div>';
+}
+
+var showModal = function() {
+  if (!JSONLoaded) {
+
+    $('#filePickerModal').modal('show');
+
+    $.ajax({
+      url: serverURL + '/data',
+      success: function(data) {
+        $('.server-selection').empty();
+        data.forEach(function(element){
+          $('.server-selection').append(makeRadioDiv(element));
+        })
+      }
+    })
+
+  } else {
+
+  }
+}
+
+var displayNetwork = function(graphData) {
+  if (!wipeOnNewLoad) wipeOnNewLoad = true;
+  var network = NetworkXGenerator(graphData);
+  render(network, {wipeOnNewLoad: wipeOnNewLoad, width: graphData.height, height: graphData.width}, graphData.graph.name);
+}
+
 var loadFile = function() {
+
+  var serverDataFile = $('input[name=optradio]:checked', '.server-selection').val();
+  if (serverDataFile) {
+    $.ajax({
+      url: serverURL + '/data/' + serverDataFile,
+      success: function(data) {
+        var parsed = JSON.parse(data);
+        displayNetwork(parsed);
+      }
+    })
+
+    return;
+  }
+
   var input, file, fr;
 
   if (typeof window.FileReader !== 'function') {
@@ -30,13 +79,13 @@ var loadFile = function() {
     fr = new FileReader();
     fr.onload = receivedText;
     fr.readAsText(file);
+    // JSONLoaded = true;
   }
 
   function receivedText(e) {
     lines = e.target.result;
     var graphData = JSON.parse(lines); 
-    if (!wipeOnNewLoad) wipeOnNewLoad = true;
-    var network = NetworkXGenerator(graphData);
-    render(network, {wipeOnNewLoad: wipeOnNewLoad, width: graphData.height, height: graphData.width}, graphData.graph.name);
+    displayNetwork(graphData);
   }
 }
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ var controller = require('./controller.js');
 var express = require('express');
 var app = express();
 
+// Allow cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/data/:name', controller.getData);
 app.get('/data/', controller.getDataList);
 
