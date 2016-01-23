4e51db13920402667b71c73407fb7536ce7b8bb9
farrukh.shahzad <farrukhshahzad2002@hotmail.com>
2016-01-23 00:22:21 -0600

    Making link as dashed line and adding a default network

---
+++ client/src/default.js
@@ -0,0 +1,75 @@
+
+/////////////////////
+//
+// default.js
+//
+// This file is responsible for the rendering of nodes on a screen, on initial load
+//
+/////////////////////
+//
+
+graphData = {
+    "graph": {
+        "name": "A Sample Network (move nodes to make a ring)",
+        "r": 250
+    },
+    "nodes": [
+        {
+            "name": "0",
+            "color": "blue",
+            "shape": "square",
+            "y": 105.0,
+            "x": 104.0,
+            "id": 0,
+            "size": 10
+        },
+        {
+            "name": "1",
+            "color": "black",
+            "shape": "circle",
+            "y": 215.0,
+            "x": 360.0,
+            "id": 1,
+            "size": 10
+        },
+        {
+            "name": "2",
+            "color": "red",
+            "shape": "square",
+            "y": 215.0,
+            "x": 260.0,
+            "id": 2,
+            "size": 10
+        },
+        {
+            "name": "3",
+            "color": "green",
+            "shape": "circle",
+            "y": 315.0,
+            "x": 10.0,
+            "id": 3,
+            "size": 15
+        }
+    ],
+    "links": [
+        {
+            "source": 0,
+            "target": 1
+        },
+        {
+            "source": 0,
+            "target": 2
+        },
+        {
+            "source": 3,
+            "target": 2
+        },
+        {
+            "source": 3,
+            "target": 1
+        }
+    ]
+}
+
+var network = NetworkXGenerator(graphData);
+render(network, {wipeOnNewLoad: true, width: graphData.graph.width, height: graphData.graph.height}, graphData.graph.name);
